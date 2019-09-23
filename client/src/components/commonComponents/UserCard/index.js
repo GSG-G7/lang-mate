d7f63fe0c7d4fcf8d541eb0d29f7c1146dd8392e
fadi <fadeomar2015@gmail.com>
2019-09-23 15:28:10 +0300

    edit the user card

--- client/src/components/commonComponents/UserCard/index.js
+++ client/src/components/commonComponents/UserCard/index.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
 const UserCard = ({
-  userName,
-  imageSrc,
-  userBio,
-  nativeLanguage,
-  learningLanguage,
+  userInfo: { avatar, username, bio, nativeLang, learningLang },
 }) => (
   <div className="userCard">
-    <img scr={imageSrc} alt="userImage" className="userCard-image" />
+    <img
+      src={avatar || 'https://img.icons8.com/clouds/2x/user.png'}
+      alt="userImage"
+      className="userCard-image"
+    />
     <div className="userCard-content">
-      <h2>{userName}</h2>
-      <p>{userBio}</p>
+      <h2>{username}</h2>
       <div className="userCard-content-languages">
         <div className="userCard-content-languages-native">
           <h3>Native</h3>
-          <h4>{nativeLanguage}</h4>
+          <h4>{nativeLang}</h4>
         </div>
         <div className="userCard-content-languages-learning">
           <h3>learning</h3>
-          <h1>{learningLanguage}</h1>
+          <h4>{learningLang}</h4>
         </div>
       </div>
     </div>
   </div>
 );
+
 UserCard.propTypes = {
-  userName: PropTypes.string.isRequired,
-  imageSrc: PropTypes.string.isRequired,
-  userBio: PropTypes.string.isRequired,
-  nativeLanguage: PropTypes.string.isRequired,
-  learningLanguage: PropTypes.string.isRequired,
+  userInfo: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    nativeLang: PropTypes.string.isRequired,
+    learningLang: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default UserCard;
