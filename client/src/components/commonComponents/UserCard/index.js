5d4d80a6aff5769b5b0f65b24be811d402ad6757
fadi <fadeomar2015@gmail.com>
2019-09-23 11:41:23 +0300

    solve eslint problems Relates #46

--- client/src/components/commonComponents/UserCard/index.js
+++ client/src/components/commonComponents/UserCard/index.js
@@ -1,19 +1,27 @@
-export default ({userName, imageSrc, userBio, nativeLanguage, learningLanguage}) => (
-    <div className="userCard">
-        <img scr={imageSrc} alt="userImage" className="userCard-image"/>
-        <div className="userCard-content">
-            <h2>{Name}</h2>
-            <p>{userBio}</p>
-            <div className="userCard-content-languages">
-                <div className="userCard-content-languages-native">
-                    <h3>Native</h3>
-                    <h4>{nativeLanguage}</h4>
-                </div>
-                <div className="userCard-content-languages-learning">
-                    <h3>learning</h3>
-                    <h1>{learningLanguage}</h1>        
-                </div>
-            </div>
+import React from 'react';
+
+export default ({
+  userName,
+  imageSrc,
+  userBio,
+  nativeLanguage,
+  learningLanguage,
+}) => (
+  <div className="userCard">
+    <img scr={imageSrc} alt="userImage" className="userCard-image" />
+    <div className="userCard-content">
+      <h2>{userName}</h2>
+      <p>{userBio}</p>
+      <div className="userCard-content-languages">
+        <div className="userCard-content-languages-native">
+          <h3>Native</h3>
+          <h4>{nativeLanguage}</h4>
         </div>
+        <div className="userCard-content-languages-learning">
+          <h3>learning</h3>
+          <h1>{learningLanguage}</h1>
+        </div>
+      </div>
     </div>
-)
+  </div>
+);
