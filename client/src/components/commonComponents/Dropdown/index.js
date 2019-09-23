9614c01d7f29bcf8cbdc8c72fa22ac242648c163
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-23 12:05:22 +0300

    fixed eslint problems in the component Relates #43

--- client/src/components/commonComponents/Dropdown/index.js
+++ client/src/components/commonComponents/Dropdown/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './index.css';
 
-const DropDown = ({label, name, languages}) => {
-    return (
-        <div className="drop-down">
-        <label for="select">{label}</label>
-        <select name={name} id="select">
-        {languages.map(({id, language}) => (
-            <option key={id} value={language}>{language}</option>
+const DropDown = ({ label, name, languages }) => {
+  return (
+    <div className="drop-down">
+      <label htmlFor="select">{label}</label>
+      <select name={name} id="select">
+        {languages.map(({ id, language }) => (
+          <option key={id} value={language}>
+            {language}
+          </option>
         ))}
-        </select>
-        </div>
-    )
-}
+      </select>
+    </div>
+  );
+};
 
 export default DropDown;
