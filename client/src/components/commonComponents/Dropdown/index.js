09100fa0b938247f3118eee4c694a62db5bf767d
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-23 10:46:28 +0300

    wrote the drop down component with the props Relates #43

---
+++ client/src/components/commonComponents/Dropdown/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+const DropDown = ({label, name, languages}) => {
+    return (
+        <div className="drop-down">
+        <label for="select">{label}</label>
+        <select name={name} id="select">
+        {languages.map(({id, language}) => (
+            <option key={id} value={language}>{language}</option>
+        ))}
+        </select>
+        </div>
+    )
+}
+
+export default DropDown;
