8b47cbea1ff768b0d034951fa30bf10125498e67
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-23 15:24:56 +0300

    added some fake data and the porp types Relates #43

--- client/src/components/commonComponents/Dropdown/index.js
+++ client/src/components/commonComponents/Dropdown/index.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 
-const DropDown = ({ label, name, languages }) => {
+const langs = [
+  {
+    id: 1,
+    language: 'Chinese',
+  },
+  {
+    id: 2,
+    language: 'Spanish',
+  },
+  {
+    id: 3,
+    language: 'English',
+  },
+  {
+    id: 4,
+    language: 'Hindi',
+  },
+  {
+    id: 5,
+    language: 'Russian',
+  },
+];
+
+const Dropdown = ({ label, name, languages = langs }) => {
   return (
     <div className="drop-down">
-      <label htmlFor="select">{label}</label>
-      <select name={name} id="select">
-        {languages.map(({ id, language }) => (
-          <option key={id} value={language}>
-            {language}
-          </option>
-        ))}
-      </select>
+      <label htmlFor="select">
+        {label}
+        <select name={name} id="select">
+          {languages.map(({ id, language }) => (
+            <option key={id} value={language}>
+              {language}
+            </option>
+          ))}
+        </select>
+      </label>
     </div>
   );
 };
 
-export default DropDown;
+Dropdown.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  languages: PropTypes.arrayOf.isRequired,
+};
+
+export default Dropdown;
