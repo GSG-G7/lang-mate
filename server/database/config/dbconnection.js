f14fcaf30de07b6a8f398bb77079fb75d0036e58
amoodaa <amoodaa@gmail.com>
2019-09-24 15:16:19 +0300

    spaced the switch

--- server/database/config/dbconnection.js
+++ server/database/config/dbconnection.js
@@ -2,12 +2,21 @@ const { Pool } = require('pg');
 require('env2')('config.env');
 
 let dbUrl = '';
+
 switch (process.env.NODE_ENV) {
-  case 'test': dbUrl = process.env.TEST_DB_URL; break;
-  case 'production': dbUrl = process.env.DATABASE_URL; break;
-  case 'development': dbUrl = process.env.DEV_DB_URL; break;
-  default: throw new Error('no db url');
+  case 'test':
+    dbUrl = process.env.TEST_DB_URL;
+    break;
+  case 'production':
+    dbUrl = process.env.DATABASE_URL;
+    break;
+  case 'development':
+    dbUrl = process.env.DEV_DB_URL;
+    break;
+  default:
+    throw new Error('no db url');
 }
+
 const options = {
   connectionString: dbUrl,
   ssl: true,
