aa4ed673518d2f97503b6cb1c7929e158cff40b3
fadi <fadeomar2015@gmail.com>
2019-09-24 18:40:37 +0300

    add index file to the conterller file Relates #20

---
+++ server/controllers/index.js
@@ -0,0 +1,15 @@
+const auth = require('./auth');
+const users = require('./users');
+const messages = require('./messages');
+const interests = require('./interests');
+const languages = require('./languages');
+const errors = require('./errors');
+
+module.exports = {
+  auth,
+  users,
+  messages,
+  interests,
+  languages,
+  errors,
+};
