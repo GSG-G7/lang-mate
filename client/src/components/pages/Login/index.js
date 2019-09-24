1bd8ed821144009ce27a020d0e39f820bc85134c
fadi <fadeomar2015@gmail.com>
2019-09-24 11:31:32 +0300

    add the elements for login page and will replace it with react components when they ready Relates #18

---
+++ client/src/components/pages/Login/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Login extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="loginPage">
+        <div className="loginBox">
+          <form action="/" className="login-form">
+            <div className="backBtn"></div>
+            <div className="login-wlecome">
+              <h1>Welcome Back</h1>
+            </div>
+            <div className="login-inpputs">
+              <input
+                type="text"
+                placeholder="username"
+                className="username-input"
+                aria-label="username"
+                name="username-input"
+              />
+              <input
+                type="text"
+                placeholder="password"
+                className="password-input"
+                name="username-password"
+                aria-label="password"
+              />
+            </div>
+            <div className="login-btn">
+              <button type="submit" className="submit-btn">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
