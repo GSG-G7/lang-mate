1d9a3f85c1c79ec846a58937669f281633160cd3
amoodaa <amoodaa@gmail.com>
2019-09-24 14:54:32 +0300

    refactored the svg into a function

--- client/src/components/common/UserMenu/index.js
+++ client/src/components/common/UserMenu/index.js
@@ -3,66 +3,38 @@ import propTypes from 'prop-types';
 import { profilePath, settingsPath, logoutPath } from './pathData';
 import './index.css';
 
+const renderSvg = (width, height, viewBox, path, fill) => (
+  <svg
+    className="user-menu__nav--svg"
+    width={width}
+    height={height}
+    viewBox={viewBox}
+    fill={fill}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path fillRule="evenodd" cpRule="evenodd" d={path} fill={fill} />
+  </svg>
+);
+
 const UserMenu = ({ username }) => (
   <div className="user-menu">
     <nav className="user-menu__nav">
       <a href={`/profile/${username}`} className="user-menu__nav--link">
-        <svg
-          className="user-menu__nav--svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            cpRule="evenodd"
-            d={profilePath}
-            fill="#7E7E7E"
-          />
-        </svg>
+        {renderSvg('24', '24', '0 0 24 24', profilePath, '#8A8A8A')}
         <span>Profile</span>
       </a>
       <a href="/settings" className="user-menu__nav--link">
-        <svg
-          className="user-menu__nav--svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            cpRule="evenodd"
-            d={settingsPath}
-            fill="#8A8A8A"
-          />
-        </svg>
+        {renderSvg('24', '24', '0 0 24 24', settingsPath, '#8A8A8A')}
         Settings
       </a>
       <a href="/logout" className="user-menu__nav--link">
-        <svg
-          className="user-menu__nav--svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            cpRule="evenodd"
-            d={logoutPath}
-            fill="#BF2929"
-          />
-        </svg>
+        {renderSvg('24', '24', '0 0 24 24', logoutPath, '#BF2929')}
         Logout
       </a>
     </nav>
   </div>
 );
+
 UserMenu.propTypes = { username: propTypes.string.isRequired };
 
 export default UserMenu;
