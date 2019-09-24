1075cfd2765c84c104e040ed093ee73b6a3e935c
amoodaa <amoodaa@gmail.com>
2019-09-24 15:08:23 +0300

    refactored renderSvg()

--- client/src/components/common/UserMenu/index.js
+++ client/src/components/common/UserMenu/index.js
@@ -4,12 +4,12 @@ import propTypes from 'prop-types';
 import { profilePath, settingsPath, logoutPath } from './pathData';
 import './index.css';
 
-const renderSvg = (width, height, viewBox, path, fill) => (
+const renderSvg = (path, fill) => (
   <svg
     className="user-menu__nav--svg"
-    width={width}
-    height={height}
-    viewBox={viewBox}
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill={fill}
     xmlns="http://www.w3.org/2000/svg"
   >
@@ -21,15 +21,15 @@ const UserMenu = ({ username }) => (
   <div className="user-menu">
     <nav className="user-menu__nav">
       <Link to={`/profile/${username}`} className="user-menu__nav--link">
-        {renderSvg('24', '24', '0 0 24 24', profilePath, '#8A8A8A')}
+        {renderSvg(profilePath, '#8A8A8A')}
         <span>Profile</span>
       </Link>
       <Link to="/settings" className="user-menu__nav--link">
-        {renderSvg('24', '24', '0 0 24 24', settingsPath, '#8A8A8A')}
+        {renderSvg(settingsPath, '#8A8A8A')}
         Settings
       </Link>
       <Link to="/logout" className="user-menu__nav--link">
-        {renderSvg('24', '24', '0 0 24 24', logoutPath, '#BF2929')}
+        {renderSvg(logoutPath, '#BF2929')}
         Logout
       </Link>
     </nav>
