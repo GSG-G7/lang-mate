53cf3afad11af996c9f4b14180cda3369a758bcb
fadi <fadeomar2015@gmail.com>
2019-09-24 18:46:56 +0300

    edit the route index to import the new files and make the route run Relates #20

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const { auth: { login, logout, signup } } = require('../controllers');
+const {
+  users: {
+    changePassword,
+    getUserChannels,
+    getUserByInterest,
+    deactivateUser,
+    getUserByLang,
+    getUserProfile,
+    searchUser,
+  },
+  messages: { addChannel },
+  interests: { getAllInterests },
+  languages: { language },
+  errors: { errors },
+} = require('../controllers');
 
-router.get('/', (req, res) => {
-  res.send({ body: 'hii from server' });
-});
+router.post('/login', login);
+router.post('/signup', signup);
+router.post('/logout', logout);
+
+router.get('/language', language);
+router.get('/interests', getAllInterests);
+router.get('/channels/:username', getUserChannels);
+router.post('/channels', addChannel);
+router.get('/users/search', searchUser);
+router.get('/users/nativeLang/:id', getUserByLang);
+router.get('/users/interest/:id', getUserByInterest);
+router.get('/users/profile/:username', getUserProfile);
+router.put('/users/deactivate', deactivateUser);
+router.put('/users/changePassword', changePassword);
+router.use(errors);
 
 module.exports = router;
