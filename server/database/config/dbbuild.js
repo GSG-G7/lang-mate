c7a126561b99ef6aeda3ea005ca09bf3e518617c
amoodaa <amoodaa@gmail.com>
2019-09-24 15:14:28 +0300

    added a new line before export for more readablity

--- server/database/config/dbbuild.js
+++ server/database/config/dbbuild.js
@@ -3,5 +3,8 @@ const { readFileSync } = require('fs');
 const dbconnection = require('./dbconnection');
 
 let sql = readFileSync(join(__dirname, 'dbbuild.sql')).toString();
-if (process.env.NODE_ENV === 'test') { sql += readFileSync(join(__dirname, 'insertFakeData')).toString(); }
+if (process.env.NODE_ENV === 'test') {
+  sql += readFileSync(join(__dirname, 'insertFakeData')).toString();
+}
+
 module.exports = () => dbconnection.query(sql);
