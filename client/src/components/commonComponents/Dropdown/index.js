3fe2717374dbf9d5b790d3c94b139b0904c51cce
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-24 10:15:31 +0300

    changed the classes names to the BEM naming style

--- client/src/components/commonComponents/Dropdown/index.js
+++ client/src/components/commonComponents/Dropdown/index.js
@@ -5,18 +5,18 @@ import './index.css';
 const Dropdown = ({ labelText, name, languages, value, onChange }) => {
   return (
     <div className="drop-down">
-      <span className="menu-desc">{labelText}</span>
+      <span className="drop-down__desc">{labelText}</span>
       <select
         name={name}
         aria-label={labelText}
         id="select"
-        className="drop-down-menu"
+        className="drop-down__menu"
         value={value}
         onChange={onChange}
       >
         {/* Creating an option for each language in the array */}
         {languages.map(({ id, language }) => (
-          <option className="drop-down-item" key={id} value={language}>
+          <option className="drop-down__item" key={id} value={language}>
             {language}
           </option>
         ))}
