1b1541554ff453e58c325009bbe15a1315ed6572
amoodaa <amoodaa@gmail.com>
2019-09-24 14:14:56 +0300

    db build changed to made tests faster

--- server/database/config/dbbuild.js
+++ server/database/config/dbbuild.js
@@ -2,8 +2,6 @@ const { join } = require('path');
 const { readFileSync } = require('fs');
 const dbconnection = require('./dbconnection');
 
-module.exports = () => {
-  let sql = readFileSync(join(__dirname, 'dbbuild.sql')).toString();
-  if (process.env.NODE_ENV === 'test') { sql += readFileSync(join(__dirname, 'insertFakeData')).toString(); }
-  return dbconnection.query(sql);
-};
+let sql = readFileSync(join(__dirname, 'dbbuild.sql')).toString();
+if (process.env.NODE_ENV === 'test') { sql += readFileSync(join(__dirname, 'insertFakeData')).toString(); }
+module.exports = () => dbconnection.query(sql);
