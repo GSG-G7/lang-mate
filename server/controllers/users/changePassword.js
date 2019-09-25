083222bdb47868695b1611f481a97dc1b7dce729
MaiUbeid <ubead95@gmail.com>
2019-09-25 19:30:24 +0300

    change the code of changePassword to a better way using Promise.all #30

--- server/controllers/users/changePassword.js
+++ server/controllers/users/changePassword.js
@@ -3,26 +3,18 @@ const { users: { changePassword, getUserById } } = require('../../database/queri
 
 exports.changePassword = (req, res, next) => {
   const { userId, oldPassword, newPassword } = req.body;
-  let userInfo;
-  getUserById(userId)
-    .then(({ rows }) => {
-      userInfo = rows;
-      return compare(oldPassword, (userInfo[0].password));
-    })
-    .then((checkOldPassword) => {
-      if (checkOldPassword) {
-        return compare(newPassword, (userInfo[0].password));
-      }
-      throw new Error('You entered a wrong password');
-    })
-    .then((checkNewPassword) => {
-      if (!checkNewPassword) {
-        hash(newPassword, 10);
-        return changePassword(userId, newPassword);
-      }
-      throw new Error('Your new password and old password are the same');
-    })
-    .then(() => res.send('Password is changed'))
+  getUserById(userId).then(({ rows }) => {
+    if (rows.length === 0) throw new Error();
+    return Promise.all([
+      compare(oldPassword, (rows[0].password)),
+      compare(newPassword, rows[0].password),
+    ]);
+  }).then(([oldPasswordMatches, newPasswordMatches]) => {
+    if (!oldPasswordMatches) throw new Error('You entered a wrong password');
+    else if (newPasswordMatches) throw new Error('Your new password and old password are the same');
+    hash(newPassword, 10);
+    return changePassword(userId, newPassword);
+  }).then(() => res.send('Password is changed'))
     .catch((err) => {
       if (err.message) next({ code: 400, msg: err.message });
       else next(err);
