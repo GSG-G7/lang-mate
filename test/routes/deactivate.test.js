303481749d241185628df67e0cb7c7a2fa3f2f45
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-25 21:18:51 +0300

    added the test for the route Relates #33

---
+++ test/routes/deactivate.test.js
@@ -0,0 +1,26 @@
+const test = require('tape');
+const supertest = require('supertest');
+const app = require('../../server/app');
+const dbBuild = require('../../server/database/config/dbbuild');
+
+test('Testing for the deactivate route', (t) => {
+  dbBuild()
+    .then(() => {
+      supertest(app)
+        .put('/api/v1/users/deactivate')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+            t.end();
+          } else {
+            t.equal(res.text.includes('deactivated'), true, 'Account should be deactivated');
+            t.end();
+          }
+        });
+    }).catch((err) => {
+      t.error(err);
+      t.end();
+    });
+});
