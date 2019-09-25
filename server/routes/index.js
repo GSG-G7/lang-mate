570d3e079adaa54aeac1a907d224bf8aa51b14fe
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-25 09:44:33 +0300

    changed the order of the lines and added a new route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,13 +6,13 @@ const {
     getUserChannels,
     getUserByInterest,
     deactivateUser,
-    getUserByLang,
-    getUserProfile,
+    getUsersByLang,
+    getUserInfo,
     searchUser,
   },
-  messages: { addChannel },
+  messages: { addChannel, getChannelMessages },
   interests: { getAllInterests },
-  languages: { language },
+  languages: { getAllLanguages },
   errors: { errors },
 } = require('../controllers');
 
@@ -20,16 +20,21 @@ router.post('/login', login);
 router.post('/signup', signup);
 router.post('/logout', logout);
 
-router.get('/language', language);
+router.get('/languages', getAllLanguages);
+
 router.get('/interests', getAllInterests);
-router.get('/channels/:username', getUserChannels);
+
+router.get('/channels/:id', getChannelMessages);
 router.post('/channels', addChannel);
+
 router.get('/users/search', searchUser);
-router.get('/users/nativeLang/:id', getUserByLang);
+router.get('/users/nativeLang/:id', getUsersByLang);
 router.get('/users/interest/:id', getUserByInterest);
-router.get('/users/profile/:username', getUserProfile);
+router.get('/users/profile/:username', getUserInfo);
 router.put('/users/deactivate', deactivateUser);
-router.put('/users/changePassword', changePassword);
+router.put('/users/change-password', changePassword);
+router.get('/users/channels/:username', getUserChannels);
+
 router.use(errors);
 
 module.exports = router;
