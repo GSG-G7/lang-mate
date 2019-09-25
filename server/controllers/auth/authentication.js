eac43aea6105797646647ee97f979ca011cfa06f
fadi <fadeomar2015@gmail.com>
2019-09-25 22:40:54 +0300

    refactor middelware for auth Realtes #38

---
+++ server/controllers/auth/authentication.js
@@ -0,0 +1,9 @@
+const { jwtVerify } = require('../../helpers/jwtVerifyPromise');
+
+const key = process.env.KEY;
+exports.auth = (req, res, next) => {
+  const { token } = req.cookies;
+  jwtVerify(token, key)
+    .then((payload) => { req.user = payload; })
+    .then(() => next());
+};
