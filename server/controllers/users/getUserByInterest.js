c1c6200227587b204c7eb6341420c768009bf526
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-25 11:10:48 +0300

    wrote the flow for the process of getting the users

--- server/controllers/users/getUserByInterest.js
+++ server/controllers/users/getUserByInterest.js
@@ -1,3 +1,10 @@
+const { } = require('../../database/queries/');
+
 exports.getUserByInterest = (req, res) => {
-  res.send('Hi from interests file');
+  const { id } = req.params;
+  // Validate the id
+  if (!Number(id)) throw new Error('Wrong interest id');
+  // Call the query
+
+  // Send the data
 };
