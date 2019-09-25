dddfeaaf2de23ae346116ec06348ce67c7723eb7
fadi <fadeomar2015@gmail.com>
2019-09-25 12:37:23 +0300

    complete the signp route Relates #27

--- server/controllers/auth/signup.js
+++ server/controllers/auth/signup.js
@@ -1,3 +1,36 @@
-exports.signup = (req, res) => {
+const { hash } = require('bcrypt');
+const { signupSchema } = require('./validation/signup-validation');
+const { users: { getUserByUsername, getUserByEmail, addUser } } = require('../../database/queries');
+
+
+exports.signup = (req, res, next) => {
+  // signupSchema.validate(userData).then(()=>)
+
   res.send(' Hello from signup file');
+  const {
+    username, email, password, native_lang_id, learning_lang_id,
+  } = req.body;
+
+  signupSchema.validate({
+    username, email, password, native_lang_id, learning_lang_id,
+  })
+    .then(() => getUserByUsername(username))
+    .then((result) => {
+      if (result.rows.length !== 0) throw Error({ err: 'username exists' });
+    })
+    .then(() => getUserByEmail(username))
+    .then((result) => {
+      if (result.rows.length !== 0) throw Error('email exists');
+    })
+    .then(() => hash(password, 10))
+    .then((hashed) => addUser({
+      username, email, password: hashed, native_lang_id, learning_lang_id,
+    }))
+    .catch(({ err }) => {
+      if (err) {
+        next(err);
+      } else {
+        next();
+      }
+    });
 };
