c7421b6af1d5ed7854cde9ee8c8591ed321bda65
fadi <fadeomar2015@gmail.com>
2019-09-25 10:58:17 +0300

    using joi vaidation for username and password and email Relates #27

--- server/controllers/auth/validation/signup-validation.js
+++ server/controllers/auth/validation/signup-validation.js
@@ -1,1 +1,19 @@
-const Joi = require('@hapi/joi')
+const Joi = require('@hapi/joi');
+
+const signupSchema = Joi.object().keys({
+  username: Joi.string()
+    .alphanum()
+    .min(2)
+    .max(30)
+    .required(),
+
+  password: Joi.string()
+    .regex(/^[a-zA-Z0-9]{8,30}$/)
+    .required(),
+  confirmPassword: Joi.ref('password'),
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .required(),
+});
+
+exports.signupValidation = (userData) => Joi.validate(userData, signupSchema);
