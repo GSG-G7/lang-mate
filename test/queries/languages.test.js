a2e2831fc56c9847006472cb959394b052db2cab
MaiUbeid <ubead95@gmail.com>
2019-09-25 13:20:01 +0300

    make the code more clear #22

--- test/queries/languages.test.js
+++ test/queries/languages.test.js
@@ -3,7 +3,14 @@ const dbbuild = require('../../server/database/config/dbbuild');
 const { languages: { getLanguages } } = require('../../server/database/queries/');
 
 test('test Languages query', (t) => {
-  const expected = [{ id: 1, name: 'arabic' }, { id: 2, name: 'english' }, { id: 3, name: 'french' }, { id: 4, name: 'spanish' }, { id: 5, name: 'dutch' }, { id: 6, name: 'japanese' }];
+  const expected = [
+    { id: 1, name: 'arabic' },
+    { id: 2, name: 'english' },
+    { id: 3, name: 'french' },
+    { id: 4, name: 'spanish' },
+    { id: 5, name: 'dutch' },
+    { id: 6, name: 'japanese' },
+  ];
   dbbuild()
     .then(() => getLanguages()
       .then((res) => res.rows)
