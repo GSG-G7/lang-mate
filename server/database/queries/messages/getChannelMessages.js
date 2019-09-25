e5290e8bc266e4dd404d05c74b849a28a21d4df4
amoodaa <amoodaa@gmail.com>
2019-09-25 13:03:07 +0300

    did the get messages of a channel query made it flexible to handle both: * get all messages * get a specific number relates #24

--- server/database/queries/messages/getChannelMessages.js
+++ server/database/queries/messages/getChannelMessages.js
@@ -1,9 +1,13 @@
 const dbConnection = require('../../config/dbConnection');
 
-exports.getChannelMessages = () => {
-  const sql = {
-    text: '',
-    value: [],
-  };
+exports.getChannelMessages = (channelId, numberOfMessages = null) => {
+  const sql = +numberOfMessages ? {
+    text: 'select * from messages where channel_id = $1 limit $2',
+    values: [channelId, numberOfMessages],
+  }
+    : {
+      text: 'select * from messages where channel_id = $1 ',
+      values: [channelId],
+    };
   return dbConnection.query(sql);
 };
