1a108904c3fc8cb809d5a5828766204f0c52e3a5
MaiUbeid <ubead95@gmail.com>
2019-09-25 16:20:49 +0300

    fix conflict in changePassword file

--- server/database/queries/users/changePassword.js
+++ server/database/queries/users/changePassword.js
@@ -3,13 +3,8 @@ const dbConnection = require('../../config/dbConnection');
 exports.changePassword = (passwordInfo) => {
   const { userId, newPassword } = passwordInfo;
   const sql = {
-<<<<<<< HEAD
     text: 'UPDATE users set password = ($2) WHERE id = ($1)',
     values: [userId, newPassword],
-=======
-    text: '',
-    values: [],
->>>>>>> 85a38d0ec46e2539bdb0c9239025ab5d7b9a0e22
   };
   return dbConnection.query(sql);
 };
