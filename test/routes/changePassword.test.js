4cfc493f159940d13b0dfa06892e0388b650345b
MaiUbeid <ubead95@gmail.com>
2019-09-25 16:27:42 +0300

    edit the travis issues

--- test/routes/changePassword.test.js
+++ test/routes/changePassword.test.js
@@ -1,24 +1,31 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../server/app');
+const dbBuild = require('../../server/database/config/dbbuild');
 
 test('Test /users/change-password route', (t) => {
-  supertest(app)
-    .put('/api/v1/users/change-password')
-    .send({
-      userId: 1,
-      newPassword: 'mai',
-      oldPassword: 'Fatma123',
-    })
-    .expect(200)
-    .expect('Content-Type', /html/)
-    .end((err, res) => {
-      if (err) {
-        t.error(err);
-        t.end();
-      } else {
-        t.equals(res.text, 'Password is changed', 'Should return password is changed');
-        t.end();
-      }
+  dbBuild()
+    .then(() => {
+      supertest(app)
+        .put('/api/v1/users/change-password')
+        .send({
+          userId: 1,
+          newPassword: 'mai',
+          oldPassword: 'Fatma123',
+        })
+        .expect(200)
+        .expect('Content-Type', /html/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+            t.end();
+          } else {
+            t.equals(res.text, 'Password is changed', 'Should return password is changed');
+            t.end();
+          }
+        });
+    }).catch((err) => {
+      t.error(err);
+      t.end();
     });
 });
