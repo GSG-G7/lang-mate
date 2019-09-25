3903c1c0944427ce8df0f3c010786160859512a5
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-25 21:16:21 +0300

    wrote the func for the put deactivate request Relates #33

--- server/controllers/users/deactivateUser.js
+++ server/controllers/users/deactivateUser.js
@@ -1,3 +1,16 @@
-exports.deactivateUser = (req, res) => {
-  res.send('Hi from deactivate file');
+const { users: { deactivateUser } } = require('../../database/queries');
+
+exports.deactivateUser = (req, res, next) => {
+  // Get the user id and username
+  // const { userId, username } = req.user;
+  // Calling the query
+  deactivateUser(1)
+    .then(({ rows: [{ isactive }] }) => {
+      const response = { id: 1, /* username, */ isactive, message: 'account deactivated' };
+      // Sending a response
+      res.status(200).json(response);
+    }).catch((err) => {
+      // Handling the error
+      next(err);
+    });
 };
