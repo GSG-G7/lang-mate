9e969961c8484d606998d54abc482da6275278f6
amoodaa <amoodaa@gmail.com>
2019-09-25 13:31:58 +0300

    did the controller for getChannelMessages route

--- server/controllers/messages/getChannelMessages.js
+++ server/controllers/messages/getChannelMessages.js
@@ -1,3 +1,15 @@
-exports.getChannelMessages = (req, res) => {
-  res.send('Hi from getting messages for a channel');
+const { messages } = require('../../database/queries');
+
+exports.getChannelMessages = (req, res, next) => {
+  messages
+    .getChannelMessages(req.params.id)
+    .then((result) => result.rows)
+    .then((rows) => res.json({
+      data:
+      {
+        channelId: req.params.id,
+        messages: rows,
+      },
+    }))
+    .catch(next);
 };
