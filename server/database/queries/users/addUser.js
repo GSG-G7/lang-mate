78abb29c5fb2d2000d161650d79b0e92cef36b9b
fadi <fadeomar2015@gmail.com>
2019-09-25 14:23:30 +0300

    add test for adduser query Rleates #27

--- server/database/queries/users/addUser.js
+++ server/database/queries/users/addUser.js
@@ -1,12 +1,13 @@
 const dbConnection = require('../../config/dbConnection');
 
 exports.addUser = (userData) => {
+  console.log(userData)
   const {
-    username, email, password, native_lang_id, learning_lang_id,
+    username, email, password, nativeLangId, learningLangId,
   } = userData;
   const sql = {
     text: 'INSERT INTO users (username, email, password, isActive, native_lang_id, learning_lang_id) VALUES ($1, $2, $3, $4, $5, $6)',
-    values: [username, email, password, true, native_lang_id, learning_lang_id],
+    values: [username, email, password, true, nativeLangId, learningLangId],
   };
   return dbConnection.query(sql);
 };
---
+++ test/queries/index.test.js
@@ -0,0 +1,1 @@
+exports.addUser = require('./addUser.test');
