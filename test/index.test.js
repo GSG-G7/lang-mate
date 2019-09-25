0b42b778a649c953a75d8829b70a37fda51a79c2
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-25 16:19:19 +0300

    added db build before all the routes tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,3 @@
 const test = require('tape');
-const { usersByInterest } = require('./routes/index.test');
-const { getUsersByInterest } = require('./queries/index.test');
-
-test('Fake initial test', (t) => {
-  t.equal(1, 1, 'should work');
-  t.end();
-});
 require('./queries/index.test');
 require('./routes/index.test');
