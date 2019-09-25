c50d8dc37fa73eef32b9af2369df37f9ea3742ad
amoodaa <amoodaa@gmail.com>
2019-09-25 16:31:46 +0300

    tests db build

--- test/routes/getChannelMessages.test.js
+++ test/routes/getChannelMessages.test.js
@@ -1,22 +1,25 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../server/app');
+const dbbuild = require('../../server/database/config/dbbuild');
 
-test('should do something', (t) => {
-  supertest(app)
-    .get('/api/v1/channels/1')
-    .expect(200)
-    .expect('Content-Type', 'application/json; charset=utf-8')
-    .end((err, res) => {
-      if (err) {
-        t.error(err);
-        t.end();
-      } else {
-        const expectedContent1 = 'hey';
-        const expectedContent2 = 'hey4';
-        t.equal(res.body.data.messages[0].content, expectedContent1, 'content should be equal');
-        t.equal(res.body.data.messages[1].content, expectedContent2, 'content should be equal');
-        t.end();
-      }
-    });
+test('should do something', async (t) => {
+  dbbuild().then(() => {
+    supertest(app)
+      .get('/api/v1/channels/1')
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .end((err, res) => {
+        if (err) {
+          t.error(err);
+          t.end();
+        } else {
+          const expectedContent1 = 'hey';
+          const expectedContent2 = 'hey4';
+          t.equal(res.body.data.messages[0].content, expectedContent1, 'content should be equal');
+          t.equal(res.body.data.messages[1].content, expectedContent2, 'content should be equal');
+          t.end();
+        }
+      });
+  }).catch(t.error);
 });
