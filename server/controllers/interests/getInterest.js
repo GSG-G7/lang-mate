8da2482567685e7faf8ff1c2e1ec34a78aebc44f
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-25 09:45:22 +0300

    removed the next from the interests file

--- server/controllers/interests/getInterest.js
+++ server/controllers/interests/getInterest.js
@@ -1,4 +1,4 @@
-exports.getAllInterests = (req, res, next) => {
+exports.getAllInterests = (req, res) => {
   // Here we will get all users with the given interest in the params
   res.send('Hi from get all interest');
 };
