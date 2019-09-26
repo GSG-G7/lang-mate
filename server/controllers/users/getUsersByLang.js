0ca77d26300e7a281e7686495d4a73628cb331e1
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-26 12:15:50 +0300

    added the func for getting the users Relates #32

--- server/controllers/users/getUsersByLang.js
+++ server/controllers/users/getUsersByLang.js
@@ -1,3 +1,25 @@
-exports.getUsersByLang = (req, res) => {
-  res.send('hi form native lang');
+const { users: { getUsersByLang }, languages: { getLanguageById } } = require('../../database/queries');
+const { formatLanguages } = require('../../helpers/formatLanguages');
+
+exports.getUsersByLang = (req, res, next) => {
+  let users;
+  const { id } = req.params;
+
+  if (!Number(id)) return next({ code: 400, msg: 'Bad request' });
+
+  getUsersByLang(id)
+    .then(({ rows }) => {
+      users = rows;
+      return users.map((user) => getLanguageById(user.native_lang_id));
+    })
+    .then((promises) => Promise.all(promises))
+    .then((langs) => formatLanguages(users, langs, 'native_lang_id'))
+    .then((result) => {
+      users = result;
+      return result.map((user) => getLanguageById(user.learning_lang_id));
+    })
+    .then((promises) => Promise.all(promises))
+    .then((langs) => formatLanguages(users, langs, 'learning_lang_id'))
+    .then((result) => res.status(200).json({ data: result }))
+    .catch((err) => next(err));
 };
