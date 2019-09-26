88d0d057d9f3e6f12ecf5038f939963cfd19ce4e
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-26 12:23:37 +0300

    saved changes after conflicts

--- test/queries/index.test.js
+++ test/queries/index.test.js
@@ -1,8 +1,5 @@
 require('./languages.test');
 require('./getChannelMessages.test');
 require('./getUsersByInterest.test');
-<<<<<<< HEAD
 require('./getUsersByLang.test');
-=======
 require('./changePassword.test');
->>>>>>> 9f13f9e0936f7d0b01f5bf792574cc67a12d4644
--- test/routes/index.test.js
+++ test/routes/index.test.js
@@ -1,8 +1,5 @@
 require('./languages.test');
 require('./getChannelMessages.test');
 require('./usersByInterest.test');
-<<<<<<< HEAD
 require('./getUsersByLang.test');
-=======
 require('./changePassword.test');
->>>>>>> 9f13f9e0936f7d0b01f5bf792574cc67a12d4644
