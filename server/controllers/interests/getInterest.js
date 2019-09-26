1998bf3823b16d6e82474e8057a28b6755729c2f
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-26 10:51:23 +0300

    refactored the middleware function

--- server/controllers/interests/getInterest.js
+++ server/controllers/interests/getInterest.js
@@ -2,9 +2,6 @@ const { interests: { getInterests } } = require('../../database/queries');
 
 exports.getAllInterests = (req, res, next) => {
   getInterests()
-    .then(({ rows }) => {
-      res.status(200).json(rows);
-    }).catch((err) => {
-      next(err);
-    });
+    .then(({ rows }) => res.status(200).json({ data: rows }))
+    .catch((err) => next(err));
 };
