4cb6389f194eca238629ce7b77ccf5a62c4bce26
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-26 12:16:45 +0300

    added the query for getting users by lang Relates #32

--- server/database/queries/users/getUsersByLang.js
+++ server/database/queries/users/getUsersByLang.js
@@ -1,9 +1,9 @@
 const dbConnection = require('../../config/dbConnection');
 
-exports.getUsersByLang = () => {
+exports.getUsersByLang = (langId) => {
   const sql = {
-    text: '',
-    values: [],
+    text: 'SELECT id, username, bio, avatar_path, native_lang_id, learning_lang_id FROM users WHERE users.native_lang_id = $1',
+    values: [langId],
   };
   return dbConnection.query(sql);
 };
