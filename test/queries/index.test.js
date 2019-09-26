d897f4baf13b7ad335677d6a066fbc68d4a4460a
fadi <fadeomar2015@gmail.com>
2019-09-26 11:35:49 +0300

    commit the tests files that income from master

--- test/queries/index.test.js
+++ test/queries/index.test.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 exports.addUser = require('./addUser.test');
-exports.languages = require('./languages.test');
-=======
 require('./languages.test');
 require('./getChannelMessages.test');
 require('./getUsersByInterest.test');
->>>>>>> d00aafce01e053991c70a97a96fd3308f55df022
