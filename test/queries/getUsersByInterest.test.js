0692d307c053b1dde45f293ef3943ca27cada778
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-26 10:00:45 +0300

    wrote the data in a better format

--- test/queries/getUsersByInterest.test.js
+++ test/queries/getUsersByInterest.test.js
@@ -4,9 +4,21 @@ const { users: { getUsersByInterest } } = require('../../server/database/queries
 
 test('Testing for the users by interest query', (t) => {
   const expected = [{
-    interest_id: 1, user_id: 1, username: 'amoodaa', avatar_path: null, bio: null, native_lang_id: 1, learning_lang_id: 6,
+    interest_id: 1,
+    user_id: 1,
+    username: 'amoodaa',
+    avatar_path: null,
+    bio: null,
+    native_lang_id: 1,
+    learning_lang_id: 6,
   }, {
-    interest_id: 1, user_id: 2, username: 'fadi', avatar_path: null, bio: null, native_lang_id: 1, learning_lang_id: 5,
+    interest_id: 1,
+    user_id: 2,
+    username: 'fadi',
+    avatar_path: null,
+    bio: null,
+    native_lang_id: 1,
+    learning_lang_id: 5,
   }];
 
   dbBuild()
