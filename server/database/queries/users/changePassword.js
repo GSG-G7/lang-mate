26659f8aa85095e95eaf0c6d6d3b541af82f7944
MaiUbeid <ubead95@gmail.com>
2019-09-26 11:22:32 +0300

    change set to SET in the query #30;

--- server/database/queries/users/changePassword.js
+++ server/database/queries/users/changePassword.js
@@ -3,7 +3,7 @@ const dbConnection = require('../../config/dbConnection');
 exports.changePassword = (passwordInfo) => {
   const { userId, newPassword } = passwordInfo;
   const sql = {
-    text: 'UPDATE users set password = $2 WHERE id = $1',
+    text: 'UPDATE users SET password = $2 WHERE id = $1',
     values: [userId, newPassword],
   };
   return dbConnection.query(sql);
