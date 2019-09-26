cdbc5ad9c887813f25a2f0fcfa27ef1fddf4572e
MaiUbeid <ubead95@gmail.com>
2019-09-26 16:28:28 +0300

    test addChannel query #40

--- test/queries/addChannel.test.js
+++ test/queries/addChannel.test.js
@@ -1,15 +1,23 @@
 const test = require('tape');
 const dbBuild = require('../../server/database/config/dbbuild');
-const { messages: { addChannel, addSubscriber } } = require('../../server/database/queries/');
+const { messages: { addChannel, addSubscriber, getChannels } } = require('../../server/database/queries/');
 
 test('test addChannel and addSubscriber query', (t) => {
   const channelName = 'Mai';
   const subscribers = [1, 2];
+  const expected = [
+    { id: 1, name: 'cha1' },
+    { id: 2, name: 'cha2' },
+    { id: 3, name: 'cha3' },
+    { id: 4, name: 'cha4' },
+    { id: 5, name: 'Mai' },
+  ];
   dbBuild()
     .then(() => addChannel(channelName))
-    .then((channelId) => addSubscriber(channelId, subscribers))
-    .then(() => {
-      // t.equals(, 'Mai', 'The password must be changed');
+    .then((result) => addSubscriber(result.rows[0].id, subscribers))
+    .then(() => getChannels())
+    .then(({ rows }) => {
+      t.deepEqual(rows, expected, 'The channel must be added');
       t.end();
     })
     .catch((err) => t.error(err));
