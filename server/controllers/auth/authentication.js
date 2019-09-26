44c19cfedbd3694a12180a8259331e5650577b2d
fadi <fadeomar2015@gmail.com>
2019-09-26 09:26:41 +0300

    add catch to the jwtVerify promise Relates #38

--- server/controllers/auth/authentication.js
+++ server/controllers/auth/authentication.js
@@ -5,5 +5,6 @@ exports.auth = (req, res, next) => {
   const { token } = req.cookies;
   jwtVerify(token, key)
     .then((payload) => { req.user = payload; })
-    .then(() => next());
+    .then(() => next())
+    .catch(() => next({ code: 403 }));
 };
