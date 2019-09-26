e43399e0a4098ebc7c89af108e6286f65cb66c2d
amoodaa <amoodaa@gmail.com>
2019-09-26 09:59:52 +0300

    made better queries that return the output, ordered and consistent relates #24

--- server/database/queries/messages/getChannelMessages.js
+++ server/database/queries/messages/getChannelMessages.js
@@ -2,11 +2,11 @@ const dbConnection = require('../../config/dbConnection');
 
 exports.getChannelMessages = (channelId, numberOfMessages = null) => {
   const sql = numberOfMessages ? {
-    text: 'select * from messages where channel_id = $1 limit $2',
+    text: 'select * from (select * from messages where channel_id = $1 order by sent_at desc) as msgs limit $2',
     values: [channelId, numberOfMessages],
   }
     : {
-      text: 'select * from messages where channel_id = $1 ',
+      text: 'select * from messages where channel_id = $1 order by sent_at desc',
       values: [channelId],
     };
   return dbConnection.query(sql);
