7246911b5daed04f6c09b09e9f53867e05949c01
MaiUbeid <ubead95@gmail.com>
2019-09-27 09:30:35 +0300

    change the text will be send in addChannel #30

--- server/controllers/messages/addChannel.js
+++ server/controllers/messages/addChannel.js
@@ -2,9 +2,10 @@ const { messages: { addChannel, addSubscriber } } = require('../../database/quer
 
 exports.addChannel = (req, res, next) => {
   const { channelName, subscribers } = req.body;
+
   addChannel(channelName)
     .then(({ rows: [{ id: channelId }] }) => addSubscriber(channelId, subscribers))
-    .then(() => res.json({ isSuccess: true, message: 'Channel is created with id:x' }))
+    .then(() => res.json({ isSuccess: true, message: 'Channel is created' }))
     // here we will continue on the socket stuff
     .catch(next);
 };
