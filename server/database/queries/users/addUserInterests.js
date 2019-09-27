f0c636f9476008354581f8e9cacadfeb782edeb7
fadi <fadeomar2015@gmail.com>
2019-09-27 20:08:45 +0300

    arranging variables Relates #27

--- server/database/queries/users/addUserInterests.js
+++ server/database/queries/users/addUserInterests.js
@@ -1,14 +1,14 @@
 const dbConnection = require('../../config/dbConnection');
 
 const interestsQueryMaker = (interests) => {
-  const query = 'INSERT INTO user_interest(interest_id, user_id) VALUES ($1,$2)';
+  const query = 'INSERT INTO user_interest(user_id, interest_id) VALUES ($1,$2)';
   const rest = interests.slice(1).map((e, i) => `, ($1,$${i + 3})`).join('');
   return query + rest;
 };
 exports.addUserInterests = (interestsIds, userId) => {
   const sql = {
     text: interestsQueryMaker(interestsIds),
-    values: [...interestsIds, userId],
+    values: [userId, ...interestsIds],
   };
   return dbConnection.query(sql);
 };
