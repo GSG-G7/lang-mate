2d52e3f8bd19425808e9903c551b9fba2d7b4cf8
MaiUbeid <ubead95@gmail.com>
2019-09-27 10:03:53 +0300

    validate channel name when its added #40

--- server/controllers/messages/addChannel.js
+++ server/controllers/messages/addChannel.js
@@ -1,9 +1,10 @@
 const { messages: { addChannel, addSubscriber } } = require('../../database/queries');
+const { validateChannel } = require('../../helpers/validateAddChannel');
 
 exports.addChannel = (req, res, next) => {
   const { channelName, subscribers } = req.body;
-
-  addChannel(channelName)
+  validateChannel.validate({ channelName })
+    .then(() => addChannel(channelName))
     .then(({ rows: [{ id: channelId }] }) => addSubscriber(channelId, subscribers))
     .then(() => res.json({ isSuccess: true, message: 'Channel is created' }))
     // here we will continue on the socket stuff
