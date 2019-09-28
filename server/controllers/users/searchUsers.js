c01baee4ae9d2b3ee8539982962738d8aa7b7160
amoodaa <amoodaa@gmail.com>
2019-09-28 15:37:02 +0300

    edited the function to use the new format func

--- server/controllers/users/searchUsers.js
+++ server/controllers/users/searchUsers.js
@@ -1,16 +1,18 @@
 const { users: { searchUsers } } = require('../../database/queries');
+const { languages: { getLanguages } } = require('../../database/queries');
+const { formatLanguagesV2 } = require('../../helpers');
 
 exports.searchUsers = (req, res, next) => {
   // we will use req.query to get results based on it
   const { search: searchQuery } = req.query;
-  searchUsers(searchQuery)
-    .then(({ rows }) => {
-      if (rows.length && rows) {
-        res.json({ data: { searchQuery, result: rows } });
-      } else {
-        throw new Error('no users with that username found :(');
+  Promise.all([searchUsers(searchQuery), getLanguages()])
+    .then(([{ rows: users }, { rows: languages }]) => {
+      console.log(users);
+      if (users && users[0]) {
+        return formatLanguagesV2(users, languages);
       }
-    })
+      throw new Error('no users with that username found :(');
+    }).then((usersFormatted) => res.json({ data: { searchQuery, result: usersFormatted } }))
     .catch((err) => {
       if (err.message === 'no users with that username found :(') {
         next({ code: 400, msg: err.message });
