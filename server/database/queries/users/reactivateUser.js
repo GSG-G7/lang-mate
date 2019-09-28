132169c0678a730472790f299cc45b8733db8684
amoodaa <amoodaa@gmail.com>
2019-09-28 14:14:25 +0300

    removed returning query

--- server/database/queries/users/reactivateUser.js
+++ server/database/queries/users/reactivateUser.js
@@ -3,7 +3,7 @@ const dbConnection = require('../../config/dbConnection');
 
 exports.reactivateUser = (userId) => {
   const sql = {
-    text: 'UPDATE users SET isactive = true WHERE users.id = $1 RETURNING isactive',
+    text: 'UPDATE users SET isactive = true WHERE users.id = $1',
     values: [userId],
   };
   return dbConnection.query(sql);
