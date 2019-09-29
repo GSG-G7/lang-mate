32a24e1b6556d76a49c725bf1f94610c33c5e46a
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 21:24:47 +0300

    added the router in the app Relates #12

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ApiService from './services/api';
 import './App.css';
+
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
+import Profile from './components/pages/Profile';
+import Landing from './components/pages/Landing';
+import Home from './components/pages/Home';
+import Chat from './components/pages/Chat';
+import Settings from './components/pages/Settings';
 
 class App extends React.Component {
   state = {
@@ -19,6 +27,16 @@ class App extends React.Component {
         <div>
           <h1>{text}</h1>
         </div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/sign-up" component={Signup} />
+          <Route path="/profile/:username" component={Profile} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/channel/:username" component={Chat} />
+          <Route path="/logout" />
+          <Route component={() => <h1>Error 404</h1>} />
+        </Switch>
       </BrowserRouter>
     );
   }
