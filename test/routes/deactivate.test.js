901f30af77a50939aff50d1fcd3f4caa60e417e0
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 13:42:49 +0300

    added the db build to the test

--- test/routes/deactivate.test.js
+++ test/routes/deactivate.test.js
@@ -2,21 +2,25 @@ const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../server/app');
 const { token } = require('./cookie.test');
+const dbBuild = require('../../server/database/config/dbbuild');
 
 test('Testing for the deactivate route', (t) => {
-  supertest(app)
-    .put('/api/v1/users/deactivate')
-    .set('Cookie', [`token=${token}`])
-    .send({ password: 'Fatma123' })
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .end((err, res) => {
-      if (err) {
-        t.error(err);
-        t.end();
-      } else {
-        t.equal(res.text.includes('deactivated'), true, 'Account should be deactivated');
-        t.end();
-      }
-    });
+  dbBuild()
+    .then(() => {
+      supertest(app)
+        .put('/api/v1/users/deactivate')
+        .set('Cookie', [`token=${token}`])
+        .send({ password: 'Fatma123' })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+            t.end();
+          } else {
+            t.equal(res.text.includes('deactivated'), true, 'Account should be deactivated');
+            t.end();
+          }
+        });
+    }).catch(next);
 });
