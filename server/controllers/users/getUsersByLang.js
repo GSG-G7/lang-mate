056965f775f9a2b8ce6bd245eb53d94b686c0648
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 14:03:28 +0300

    wrote the function in a better format

--- server/controllers/users/getUsersByLang.js
+++ server/controllers/users/getUsersByLang.js
@@ -2,18 +2,12 @@ const { users: { getUsersByLang }, languages: { getLanguages } } = require('../.
 const { formatLanguagesV2 } = require('../../helpers');
 
 exports.getUsersByLang = (req, res, next) => {
-  let users;
   const { id } = req.params;
 
   if (!Number(id)) return next({ code: 400, msg: 'Bad request' });
 
-  getUsersByLang(id)
-    .then(({ rows }) => {
-      users = rows;
-      return users;
-    })
-    .then(() => getLanguages())
-    .then((langs) => formatLanguagesV2(users, langs.rows))
+  Promise.all([getUsersByLang(id), getLanguages()])
+    .then(([{ rows: users }, { rows: langs }]) => formatLanguagesV2(users, langs))
     .then((result) => res.status(200).json({ data: result }))
     .catch((err) => next(err));
 };
