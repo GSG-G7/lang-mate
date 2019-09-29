5fd6f19ed4523e0d5bc0b7871b65a1097bedb9c9
fadi <fadeomar2015@gmail.com>
2019-09-29 14:03:02 +0300

    add onclickk functions on input filed at the api service file

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,3 +1,5 @@
 const fetchData = () => fetch('/api/v1').then(res => res.json());
-const login = () => console.log('make fetch');
-export default { fetchData, login };
+const loginFun = () => console.log('onClick function at the logiBtn element');
+const inputFun = () => console.log('onClick function at the input element');
+const messErrFun = () => console.log('err message for validation');
+export default { fetchData, loginFun, inputFun, messErrFun };
