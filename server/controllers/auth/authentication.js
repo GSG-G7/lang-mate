6fc1e95dda6d2fc047cc9a89288986081315334f
fadi <fadeomar2015@gmail.com>
2019-09-29 13:05:26 +0300

    edit auth file and edit the status code

--- server/controllers/auth/authentication.js
+++ server/controllers/auth/authentication.js
@@ -3,8 +3,12 @@ const { jwtVerify } = require('../../helpers/jwtVerifyPromise');
 const key = process.env.KEY;
 exports.auth = (req, res, next) => {
   const { token } = req.cookies;
-  jwtVerify(token, key)
-    .then((payload) => { req.user = payload; })
-    .then(() => next())
-    .catch(() => next({ code: 403 }));
+  if (req.cookies && token) {
+    jwtVerify(token, key)
+      .then((payload) => { req.user = payload; })
+      .then(() => next())
+      .catch(() => next({ code: 401 }));
+  } else {
+    next({ code: 401 });
+  }
 };
