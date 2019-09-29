4208ab6fd707a8f2110af78374aa6a6b9fd201c6
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 13:08:40 +0300

    made the deactivate test work

--- server/controllers/users/deactivateUser.js
+++ server/controllers/users/deactivateUser.js
@@ -2,11 +2,13 @@ const { users: { deactivateUser } } = require('../../database/queries');
 
 exports.deactivateUser = (req, res, next) => {
   // Get the user id and username
-  // const { id, username } = req.user;
+  const { userInfo: { id, username } } = req.user;
   // Calling the query
-  deactivateUser(1)
+  deactivateUser(id)
     .then(({ rows: [{ isactive }] }) => {
-      const response = { id: 1, /* username, */ isactive, message: 'account deactivated' };
+      const response = {
+        id: 1, username, isactive, message: 'account deactivated',
+      };
       // Sending a response
       res.status(200).json(response);
     }).catch((err) => {
--- test/routes/deactivate.test.js
+++ test/routes/deactivate.test.js
@@ -1,26 +1,21 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../server/app');
-const dbBuild = require('../../server/database/config/dbbuild');
+const { token } = require('./cookie.test');
 
 test('Testing for the deactivate route', (t) => {
-  dbBuild()
-    .then(() => {
-      supertest(app)
-        .put('/api/v1/users/deactivate')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end((err, res) => {
-          if (err) {
-            t.error(err);
-            t.end();
-          } else {
-            t.equal(res.text.includes('deactivated'), true, 'Account should be deactivated');
-            t.end();
-          }
-        });
-    }).catch((err) => {
-      t.error(err);
-      t.end();
+  supertest(app)
+    .put('/api/v1/users/deactivate')
+    .set('Cookie', [`token=${token}`])
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        t.error(err);
+        t.end();
+      } else {
+        t.equal(res.text.includes('deactivated'), true, 'Account should be deactivated');
+        t.end();
+      }
     });
 });
