4dc6e51c914f54dd28c371c8f9909818a0a7d5e5
fadi <fadeomar2015@gmail.com>
2019-09-29 11:54:08 +0300

    edit login function in api servieces

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,3 +1,3 @@
 const fetchData = () => fetch('/api/v1').then(res => res.json());
-const login = (username, password) => <div>hi</div>;
+const login = () => console.log('make fetch');
 export default { fetchData, login };
