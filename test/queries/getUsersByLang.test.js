e19ef9b5b7e08899c30e9f9d680ab2239bfec486
amoodaa <amoodaa@gmail.com>
2019-09-29 13:54:25 +0300

    Edit the query to return interests too Relates : #32

--- test/queries/getUsersByLang.test.js
+++ test/queries/getUsersByLang.test.js
@@ -6,10 +6,12 @@ test('Testing for the users by language query', (t) => {
   const expected = {
     id: 1,
     username: 'amoodaa',
+    email: 'amoodaa@gmail.com',
     bio: null,
     avatar_path: null,
     native_lang_id: 1,
     learning_lang_id: 6,
+    interests: [{ id: 1, name: 'music' }, { id: 2, name: 'sports' }],
   };
   dbBuild()
     .then(() => getUsersByLang(1))
