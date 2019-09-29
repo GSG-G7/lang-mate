e836bcd77ac37e1235eea812945f402095a40cdd
amoodaa <amoodaa@gmail.com>
2019-09-29 15:15:30 +0300

    tested reactvation relates #97

--- test/routes/login.test.js
+++ test/routes/login.test.js
@@ -2,6 +2,7 @@ const test = require('tape');
 const supertest = require('supertest');
 const app = require('../../server/app');
 const dbBuild = require('../../server/database/config/dbbuild');
+const { token } = require('./cookie.test');
 
 test('Test /login route', (t) => {
   const userInfo = {
@@ -24,4 +25,43 @@ test('Test /login route', (t) => {
         }
       });
   }).catch(t.error);
+});
+
+
+test('Testing for the reactivating action on /login route', (t) => {
+  const userInfo = {
+    username: 'fadi',
+    password: 'Fatma123',
+  };
+  dbBuild()
+    .then(() => {
+      supertest(app)
+        .put('/api/v1/users/deactivate')
+        .set('Cookie', [`token=${token}`])
+        .send({ password: 'Fatma123' })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          if (err) {
+            t.error(err);
+            t.end();
+          } else {
+            t.equal(res.body.message, 'account is deactivated');
+            supertest(app)
+              .post('/api/v1/login')
+              .send(userInfo)
+              .expect(200)
+              .expect('Content-Type', 'application/json; charset=utf-8')
+              .end((err2, res2) => {
+                if (err2) {
+                  t.error(err2);
+                  t.end();
+                } else {
+                  t.ok(res2.res.headers['set-cookie'][0].includes('token='), 'login activated!');
+                  t.end();
+                }
+              });
+          }
+        });
+    }).catch((err) => t.error(err));
 });
