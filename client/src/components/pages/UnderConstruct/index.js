061630576a4103a5272350555de02e8751639d9f
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 20:51:44 +0300

    added the under construct and its styles Relates #112

---
+++ client/src/components/pages/UnderConstruct/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+const UnderConstruct = () => {
+  return (
+    <div className="under-construct">
+      <h2 className="under-construct__heading">this page is under construct</h2>
+      <p className="under-construct__text">
+        Our developers are currently working on this page
+      </p>
+    </div>
+  );
+};
+
+export default UnderConstruct;
