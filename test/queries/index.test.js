ea9517239154b54ba16074d2b10d1ad64f08b035
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 10:51:16 +0300

    fixed merging conflicts in test files

--- test/queries/index.test.js
+++ test/queries/index.test.js
@@ -3,11 +3,8 @@ require('./getInterests.test');
 require('./languages.test');
 require('./getChannelMessages.test');
 require('./getUsersByInterest.test');
-<<<<<<< HEAD
 require('./getUsersByLang.test');
-=======
 require('./searchUsers.test');
->>>>>>> d5181da78747bfd1413057d53de4d258ca76bd73
 require('./changePassword.test');
 require('./getChannels.test');
 require('./addChannel.test');
