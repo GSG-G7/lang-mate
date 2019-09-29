1718a7a47773b762ddc33957952e6f6949be3684
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-29 14:42:32 +0300

    added the cookie in the test file

--- test/routes/getUsersByLang.test.js
+++ test/routes/getUsersByLang.test.js
@@ -1,8 +1,10 @@
 const test = require('tape');
 const supertest = require('supertest');
 
+const { token } = require('./cookie.test');
 const app = require('../../server/app');
 const dbBuild = require('../../server/database/config/dbbuild');
+
 
 test('Testing for the users by lang route', (t) => {
   dbBuild()
@@ -11,6 +13,7 @@ test('Testing for the users by lang route', (t) => {
         .get('/api/v1/users/native-lang/1')
         .expect(200)
         .expect('Content-Type', /json/)
+        .set('Cookie', [`token=${token}`])
         .end((err, res) => {
           if (err) {
             t.error(err);
