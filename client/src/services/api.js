e3ee4e45a019534a2e8c56edb560e89961ffb5f9
fadi <fadeomar2015@gmail.com>
2019-09-29 11:26:20 +0300

    add login function in the services api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,3 +1,3 @@
 const fetchData = () => fetch('/api/v1').then(res => res.json());
-
-export default { fetchData };
+const login = (username, password) => <div>hi</div>;
+export default { fetchData, login };
