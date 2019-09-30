d19eb0818c2d5e3fcd1662520a178ff60d6352c0
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-30 09:59:36 +0300

    changed it to under construction Relates #112

--- client/src/components/pages/UnderConstruction/index.js
+++ client/src/components/pages/UnderConstruction/index.js
@@ -4,7 +4,9 @@ import './index.css';
 const UnderConstruct = () => {
   return (
     <div className="under-construct">
-      <h2 className="under-construct__heading">this page is under construct</h2>
+      <h2 className="under-construct__heading">
+        this page is under construction
+      </h2>
       <p className="under-construct__text">
         Our developers are currently working on this page
       </p>
