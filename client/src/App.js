b625d967d54df0ee342ccd1f9fe61972732f0b1c
MaiUbeid <ubead95@gmail.com>
2019-09-30 11:18:34 +0300

    fix conflict in signup page #31

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter, Route } from 'react-router-dom';
-=======
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
->>>>>>> fa5f214ef7c0be906340700ec500aae096d07884
 import ApiService from './services/api';
 import Signup from './components/pages/Signup';
 import './App.css';
@@ -29,12 +25,6 @@ class App extends React.Component {
     const { isLogged } = this.state;
     return (
       <BrowserRouter>
-<<<<<<< HEAD
-        <div>
-          <h1>{text}</h1>
-          <Route exact path="/sign-up" component={Signup} />
-        </div>
-=======
         <Switch>
           <Route exact path="/" component={isLogged ? Home : Landing} />
           <Route path="/login" component={Login} />
@@ -45,7 +35,6 @@ class App extends React.Component {
           <Route path="/logout" />
           <Route component={() => <h1>Error 404</h1>} />
         </Switch>
->>>>>>> fa5f214ef7c0be906340700ec500aae096d07884
       </BrowserRouter>
     );
   }
