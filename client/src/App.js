446a8ad297f743c1c32f77fc33f4e35142836554
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-30 10:12:20 +0300

    added the under construction to all routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Landing from './components/pages/Landing';
 import Home from './components/pages/Home';
 import Chat from './components/pages/Chat';
 import Settings from './components/pages/Settings';
+import UnderConstruction from './components/pages/UnderConstruction';
 
 class App extends React.Component {
   state = {
@@ -17,7 +18,7 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    ApiService.fetchData().then(result => this.setState({ text: result.body }));
+    // ApiService.fetchData().then(result => this.setState({ text: result.body }));
   }
 
   render() {
@@ -28,12 +29,12 @@ class App extends React.Component {
           <h1>{text}</h1>
         </div>
         <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/login" component={Login} />
-          <Route path="/sign-up" component={Signup} />
-          <Route path="/profile/:username" component={Profile} />
-          <Route path="/settings" component={Settings} />
-          <Route path="/channel/:username" component={Chat} />
+          <Route exact path="/" component={UnderConstruction} />
+          <Route path="/login" component={UnderConstruction} />
+          <Route path="/sign-up" component={UnderConstruction} />
+          <Route path="/profile/:username" component={UnderConstruction} />
+          <Route path="/settings" component={UnderConstruction} />
+          <Route path="/channel/:username" component={UnderConstruction} />
           <Route path="/logout" />
           <Route component={() => <h1>Error 404</h1>} />
         </Switch>
