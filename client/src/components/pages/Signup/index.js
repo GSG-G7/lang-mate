7eb97f64c40c71b5a40cec38ff17116f5eb8d205
Fatma Siam <fatma.o.siam@gmail.com>
2019-10-02 00:53:20 +0300

    move the page under constarction from pages and put it on the common

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import UnderConstruction from '../UnderConstruction';
+import UnderConstruction from '../../common/UnderConstruction';
 
 const Signup = () => <UnderConstruction />;
 
