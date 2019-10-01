3fd2748d8d1ad8c7e22e413832b08f9f180e1536
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-01 09:52:49 +0300

    changed classes names and added spans

--- client/src/components/common/UserCard/index.js
+++ client/src/components/common/UserCard/index.js
@@ -3,24 +3,28 @@ import PropTypes from 'prop-types';
 import './index.css';
 
 const UserCard = ({
-  userInfo: { avatar, username, nativeLang, learningLang },
+  userInfo: { avatar_path: avatar, username, nativeLang, learningLang },
 }) => (
   <div className="user-card">
     <img
-      src={avatar || 'https://img.icons8.com/clouds/2x/user.png'}
+      src={
+        avatar ||
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80'
+      }
       alt="userImage"
       className="user-card__image"
     />
+
     <div className="user-card__content">
-      <h2>{username}</h2>
-      <div className="user-card__content__languages">
-        <div className="user-card__content__languages-native">
-          <h4>Native</h4>
-          <h2>{nativeLang}</h2>
+      <h2 className="user-card__username">{username}</h2>
+      <div className="user-card__languages">
+        <div className="user-card__content__native">
+          <span className="user-card__content__span">Native</span>
+          <h3 className="user-card__content__language">{nativeLang.name}</h3>
         </div>
-        <div className="user-card__content__languages-learning">
-          <h4>learning</h4>
-          <h2>{learningLang}</h2>
+        <div className="user-card__content__learning">
+          <span className="user-card__content__span">learning</span>
+          <h3 className="user-card__content__language">{learningLang.name}</h3>
         </div>
       </div>
     </div>
@@ -30,9 +34,9 @@ const UserCard = ({
 UserCard.propTypes = {
   userInfo: PropTypes.shape({
     username: PropTypes.string.isRequired,
-    avatar: PropTypes.string,
-    nativeLang: PropTypes.string.isRequired,
-    learningLang: PropTypes.string.isRequired,
+    avatar_path: PropTypes.string,
+    nativeLang: PropTypes.objectOf.isRequired,
+    learningLang: PropTypes.objectOf.isRequired,
   }).isRequired,
 };
 
