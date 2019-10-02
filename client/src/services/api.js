8175b6db69e3a348ce445595dc2d746e2c2c856d
fadi <fadi@pop-os.localdomain>
2019-10-02 11:22:16 +0300

    add fetch for signup route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -8,4 +8,13 @@ export default {
       body: JSON.stringify(data),
     }).then(res => res.json());
   },
+  signUp: data => {
+    return fetch('api/v1/sign-up', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(res => res.json());
+  },
 };
