a603d8925efc5c1288de958d1e81270c7553b81e
fadi <fadi@pop-os.localdomain>
2019-10-02 11:22:47 +0300

    add validation for the first step of sign up

--- client/src/components/pages/utils/signupValidation.js
+++ client/src/components/pages/utils/signupValidation.js
@@ -12,7 +12,7 @@ const signupValidation = yup.object().shape({
     .trim('space is invalid'),
   password: yup
     .string()
-    .min(10, 'Password has to be longer than 10 characters')
+    .min(6, 'Password has to be longer than 6 characters')
     .matches(/(?=.*[0-9])/, 'Password must contain a number')
     .required('Password is required!'),
   confirmPassword: yup
