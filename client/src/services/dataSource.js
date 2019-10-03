c7edefde58ff6bca570a036c1334b0873eb0e80f
amoodaa <amoodaa@gmail.com>
2019-10-03 08:54:30 +0300

    reedited per jan comments #137

--- client/src/services/dataSource.js
+++ client/src/services/dataSource.js
@@ -1,22 +1,17 @@
 import api from './api';
 
-const DataSource = {
-  data: {
-    loggedUser: {},
-    users: [],
-    channels: [],
-  },
-  subscribers: [],
-  addChangeListener: handleChange => {
-    if (DataSource.subscribers.indexOf(handleChange) === -1)
-      DataSource.subscribers.push(handleChange);
-  },
-  removeChangeListener: handleChange => {
-    DataSource.subscribers = DataSource.subscribers.filter(
-      e => e !== handleChange
-    );
-  },
-  onChange: () => DataSource.subscribers.forEach(fun => fun()),
+const data = {
+  loggedUser: {},
+  users: [],
+  channels: [],
 };
 
-export default DataSource;
+const subscribers = new Set();
+
+const DataSource = {
+  addChangeListener: notfiyFn => subscribers.add(notfiyFn),
+  removeChangeListener: notfiyFn => subscribers.delete(notfiyFn),
+  onChange: () => subscribers.forEach(notify => notify()),
+};
+
+export default Object.freeze(DataSource);
