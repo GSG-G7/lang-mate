6fbe766beb19b99775a71bbfcd0707922a296ad4
Fatma Siam <fatma.o.siam@gmail.com>
2019-10-03 09:55:31 +0300

    edit the auth

--- client/src/components/Auth/PrivateRoute.js
+++ client/src/components/Auth/PrivateRoute.js
@@ -14,7 +14,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         return (
           <Redirect
             to={{
-              pathname: '/',
+              pathname: '/login',
               state: { from: props.location },
             }}
           />
