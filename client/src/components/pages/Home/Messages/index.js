4988ae91762b3fb7f7149a5a3049713116360d83
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-03 11:28:06 +0300

    transfered the link from the messages to message c

--- client/src/components/pages/Home/Messages/index.js
+++ client/src/components/pages/Home/Messages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import MessageCard from '../../../common/MessageCard';
 import './index.css';
 
@@ -8,19 +7,14 @@ const Messages = ({ messages: { user, channels, users } }) => {
   return (
     <div>
       {channels.map(channel => (
-        <Link
-          to={{
-            pathname: `/channel/${channel.channelId}`,
-          }}
-        >
-          <MessageCard
-            key={channel.channelId}
-            participant={
-              users.find(({ id }) => id === channel.participants[0]) || user
-            }
-            lastMessage={channel.messages[channel.messages.length - 1]}
-          />
-        </Link>
+        <MessageCard
+          key={channel.channelId}
+          channel={channel}
+          participant={
+            users.find(({ id }) => id === channel.participants[0]) || user
+          }
+          lastMessage={channel.messages[channel.messages.length - 1]}
+        />
       ))}
     </div>
   );
