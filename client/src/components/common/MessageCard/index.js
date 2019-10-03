bd2e45b07d63ae3ba079a6e09b8972a74c766f33
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-03 11:27:17 +0300

    added channel id in the message card

--- client/src/components/common/MessageCard/index.js
+++ client/src/components/common/MessageCard/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import convertMs from '../../../services/date';
 import './index.css';
 
 const MessageCard = ({
+  channel: { channelId },
   participant: { avatar_path: avatar, username },
   lastMessage: { content, sent_at: timestamp },
 }) =>
   username ? (
-    <div className="message-card">
-      <img
-        src={avatar || 'https://img.icons8.com/clouds/2x/user.png'}
-        alt={`${username} avatar`}
-        className="message-card__image"
-      />
-      <div className="message-card__content">
-        <h2 className="message-card__username">{username}</h2>
-        <p className="message-card__message">{content}</p>
+    <Link
+      to={{
+        pathname: `/channel/${username}/${channelId}`,
+      }}
+    >
+      <div className="message-card">
+        <img
+          src={avatar || 'https://img.icons8.com/clouds/2x/user.png'}
+          alt={`${username} avatar`}
+          className="message-card__image"
+        />
+        <div className="message-card__content">
+          <h2 className="message-card__username">{username}</h2>
+          <p className="message-card__message">{content}</p>
+        </div>
+        <div className="message-card__time">
+          <span className="message-card__time-span">
+            {convertMs(Date.now() - new Date(timestamp)).hour} hour
+          </span>
+        </div>
       </div>
-      <div className="message-card__time">
-        <span className="message-card__time-span">
-          {convertMs(Date.now() - new Date(timestamp)).hour} hour
-        </span>
-      </div>
-    </div>
+    </Link>
   ) : (
     ''
   );
